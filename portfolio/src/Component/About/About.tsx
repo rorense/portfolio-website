d4989a2f0a0baf10d4055bb7b2974da5b38f3de8
rorense <rkorense@gmail.com>
2023-10-22 22:26:46 +1300

    work

--- portfolio/src/Component/About/About.tsx
+++ portfolio/src/Component/About/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import about from "./About.module.css";
 
-// The about component
+// The about component.
 export const About: React.FC = () => {
   return (
     <div className={about.about}>
